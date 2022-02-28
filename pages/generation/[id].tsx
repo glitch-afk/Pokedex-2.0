ffe0036b0e9ae02c46a9503ace3202d1cd830d4c
v3n0m <rahul004@protonmail.ch>
2022-02-28 23:55:15 +0530

    Some restructuring

--- pages/generation/[id].tsx
+++ pages/generation/[id].tsx
@@ -1,26 +1,70 @@
-import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import {
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+  GetStaticPaths,
+} from "next";
 
-type Props = {};
+import type { genData } from "../index";
 
-const Gen = (props: Props) => {
-  const [pokemons, setPokemons] = useState([]);
-  const router = useRouter();
-  console.log("router", router);
+import PokeCard from "../../components/PokeCard";
 
-  const getAllPokemons = async (url: string) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-  };
+const Gen = ({
+  genNumData,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(genNumData.pokemon_species);
 
-  useEffect(() => {
-    getAllPokemons(
-      `https://pokeapi.co/api/v2/generation/${router.query.id}`
-    ).then((data) => console.log(data.pokemon_species));
-  }, []);
-
-  return <>Hello dynamic route</>;
+  return (
+    <div className="flex items-center justify-center w-full h-fit">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-8 gap-y-8 mx-10 mt-4 mb-12">
+        {genNumData.pokemon_species.map((pokemon: genData) => (
+          <PokeCard key={pokemon.name}>
+            <h1>{pokemon.name}</h1>
+          </PokeCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Gen;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch(`https://pokeapi.co/api/v2/generation/`);
+  const data = await res.json();
+
+  const paths = data.results.map((gen: genData) => ({
+    params: { id: gen.name },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const { params } = context;
+
+  const inValidGen = {
+    name: "invalid",
+    url: "",
+  };
+
+  if (!params?.id) {
+    return {
+      props: {
+        gen: inValidGen,
+      },
+    };
+  }
+  const allGenPokemon = await fetch(
+    `https://pokeapi.co/api/v2/generation/${params.id}`
+  );
+  const genNumData = await allGenPokemon.json();
+
+  return {
+    props: {
+      genNumData,
+    },
+  };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
-import type { NextPage } from "next";
-import Generation from "./Generation";
+import { InferGetStaticPropsType } from "next";
+import Generation from "../components/Generation";
 
-const Home: NextPage = () => {
+const Home = ({ GenData }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(GenData);
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-6xl my-10 text-[#d8dee9]">Pokedex</h1>
-      <Generation />
+      <Generation GenData={GenData} />
     </div>
   );
 };
 
 export default Home;
+
+export type genData = {
+  name: string;
+  url: string;
+};
+
+export const getStaticProps = async () => {
+  const pokemonGen = await fetch("https://pokeapi.co/api/v2/generation/");
+  const Gendata = await pokemonGen.json();
+  return {
+    props: {
+      GenData: Gendata.results,
+    },
+  };
+};
